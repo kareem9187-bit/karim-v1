2ea1b7edd3e937eef379607a7c4d5a4ce4ca3cf1
extacademyedu-lang <Muhammedmekky4@gmail.com>
2026-06-06 05:43:36 +0300

    feat: add floating CTA with back-to-top and let's talk states

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,5 +1,6 @@
 import { Navbar } from '@/components/public/Navbar';
 import { Footer } from '@/components/public/Footer';
+import { FloatingCTA } from '@/components/public/FloatingCTA';
 
 export default function PublicLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -15,6 +16,9 @@ export default function PublicLayout({ children }: { children: React.ReactNode }
 
       {/* Page content */}
       <main className="relative z-10">{children}</main>
+
+      {/* Floating CTA */}
+      <FloatingCTA />
 
       {/* Footer */}
       <Footer />
